e7c14584f1741b084d7ffbad52a0aedd8d591256
Sergiosirena17 <113640957+Sergiosirena17@users.noreply.github.com>
2023-12-06 13:58:38 -0500

    eliminar.php

--- eliminar.php
+++ eliminar.php
@@ -1,46 +1,14 @@
+<?php 
+    include("conexion_be.php");
+        $id = $_GET['id'];
+        $sql = "delete from juego where id = '".$id."'";
+        $resultado = mysqli_query($conexion,$sql);
+            if($resultado){
+                echo "<script>alert('LOS DATOS SE ELIMINARON CORRECTAMENTE');location.assign('bienvenido.php');</script>";
+            }
+            else{
+            echo "<script> alert('LOS DATOS NO FUERON ACTUALIZADOS CORRECTAMENTE, POR FAVOR REVISE QUE SUCEDIÓ');location.assign('bienvenido.php');</script>"; 
+            }
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <title>Tienda</title>
-    <link rel="stylesheet" href="styletemplate.css">
-    <style>
-        body {
-          margin: 0;
-          padding: 0;
-          font-family: 'Arial', sans-serif;
-        }
-    
-        .contenido {
-          padding: 20px;
-          color: #fff;
-        }
-      </style>
-</head>
-
-<body style="background-image: url('play4.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat;">
-    
-    <header>
-            <div class="menu">
-                <div><img src="icons8-videogame-64.png"></div>
-                <div><a href="">Tienda</a></div> 
-                <div><a href="">Productos</a></div> 
-                <div><a href="">Oferta</a></div>  
-                <div><a href="login.php">Manager</a></div>
-                <div><img src = "icons8-carrito-de-compras-48.png"></div>
-            </div>
-    </header>
-<link rel="stylesheet" href="stylemain.css">
-<div class="container" id="aa">
-</div>
-<footer>
-  <p> Copyright tienda 2023</p>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-<script src="scriptmain.js"></script>
-</body>
-</html>
+    mysqli_close($conexion);
+?>
